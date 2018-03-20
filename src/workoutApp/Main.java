75046c06c3206a2fd83ffbe3f154713cda2bbe9b
Peder Breivik Grundvold <pederbg@stud.ntnu.no>
2018-03-20 18:12:28 +0100

    fix gitignore and pom.xml and add interface for text based app

--- src/workoutApp/Main.java
+++ src/workoutApp/Main.java
@@ -2,16 +2,15 @@ package workoutApp;
 
 
 import java.util.ArrayList;
+import java.util.Scanner; 
 
 import org.joda.time.DateTime;
 import org.joda.time.LocalTime;
 
 
-
-
-
 public class Main {
 	
+	//--------------------------------------------------------------------------------------
 	public void registerEquipment(String name,String description) { //oppg 1
 		UseDB.addRow("equipment",name,description);
 	}
@@ -19,7 +18,7 @@ public class Main {
 		UseDB.addRow("exercise",name,description);
 	}
 	public void registerWorkout(int year, int month, int day,int hour,int minute, int second,String info,int shape,int performance,String note) {
-		DateTime date = new DateTime(year,month,day,0,0,0);   //oppg 1 (litt usikker på om mySQL godtar date og time)
+		DateTime date = new DateTime(year,month,day,0,0,0);   //oppg 1 (litt usikker pï¿½ om mySQL godtar date og time)
 		LocalTime time = new LocalTime(hour,minute,second);
 		
 		UseDB.addRow("workout",date,time,info,shape,performance,note);
@@ -29,7 +28,7 @@ public class Main {
 	}
 	
 	public ArrayList<ArrayList<String>> latestWorkouts(int n) { //oppg 2
-		ArrayList<ArrayList<String>> latestWorkouts = UseDB.getTable("SELECT * FROM workout ORDER by (date_time) DESC limit 0,"+n+";"); //Funker, men burde legge til flere workouts for å være sikker
+		ArrayList<ArrayList<String>> latestWorkouts = UseDB.getTable("SELECT * FROM workout ORDER by (date_time) DESC limit 0,"+n+";"); //Funker, men burde legge til flere workouts for ï¿½ vï¿½re sikker
 		return latestWorkouts;
 	}
 	public ArrayList<ArrayList<String>> resultLogs(String exercise,int year1,int month1,int day1,int year2,int month2,int day2) { //oppg 3
@@ -39,23 +38,69 @@ public class Main {
 		return resultLogs;
 	}
 	public ArrayList<ArrayList<String>> similarExercises() { //oppg 4
-		 ArrayList<ArrayList<String>> similarExercises = UseDB.getTable("SQL spørring"); //feil sql syntax
+		 ArrayList<ArrayList<String>> similarExercises = UseDB.getTable("SQL spï¿½rring"); //feil sql syntax
 			return similarExercises;
 		}
 	public ArrayList<ArrayList<String>> numberOfWorkouts() { //oppg 4
 		ArrayList<ArrayList<String>> numberOfWorkouts = UseDB.getTable("SELECT COUNT(*) FROM workout"); //denne funker:)
 			return numberOfWorkouts;
+		}	
+	//--------------------------------------------------------------------------------------
+	
+	// Text based app interface
+	public void textAppLoop() {
+		Scanner scan = new Scanner(System.in);
+		String in = "";
+		System.out.println("This is a text based workout app.\n"
+				+ "COMMANDS:\n"
+				+ "   - Regeister equipment: REGEQ\n"
+				+ "   - Regeister exercise: REGEX\n"
+				+ "   - Regeister workout: REGWORK\n"
+				+ "TODO"
+				+ "TODO"
+				+ "TODO"
+				+ "   - Exit of of the app: QUIT or EXIT\n");
+		while (true) {
+			try {
+				in = scan.nextLine().toUpperCase();
+			}catch (Exception e) {
+				System.out.println("Invalid input");
+			}
+			switch (in) {
+			
+				case "REGEQ":
+					System.out.println("todo regeq");
+					break;
+				case "REGEX":
+					System.out.println("todo regex");
+					break;
+				case "REGWORK":
+					System.out.println("todo regwork");
+					break;
+					
+				case "QUIT":
+					System.out.println("Goodbye");
+					System.exit(0);
+				case "EXIT":
+					System.out.println("Goodbye");
+					System.exit(0);
+					
+				default:
+					System.out.println("Not a command");
+			
+			}
 		}
+	}
 	
 	public static void main(String[] args) {
-		System.out.println("halla gutta");
+		Main test = new Main();
 		
-		ArrayList<ArrayList<String>> test = UseDB.getTable("SELECT * FROM exercise");
+		/*ArrayList<ArrayList<String>> test = UseDB.getTable("SELECT * FROM exercise");
 		System.out.println(test);
+				
+		System.out.println(test2.resultLogs("pullups", 2018, 1, 1, 2018, 2, 28));*/
 		
-		Main test2 = new Main();
-		
-		System.out.println(test2.resultLogs("pullups", 2018, 1, 1, 2018, 2, 28));
+		test.textAppLoop();
 	}
 
 }
