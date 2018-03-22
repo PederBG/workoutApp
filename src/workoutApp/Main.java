e04abfe385e4998a41c6299d0f0e38b19e2dadc4
Larsern <laberge@stud.ntnu.no>
2018-03-22 13:18:33 +0100

    Oppg4, får nå ut alle øvelser i samme gruppe

--- src/workoutApp/Main.java
+++ src/workoutApp/Main.java
@@ -2,37 +2,37 @@ package workoutApp;
 
 
 import java.util.ArrayList;
-import java.util.Scanner; 
+import java.util.Scanner;
 
 import org.joda.time.DateTime;
 import org.joda.time.LocalTime;
 
 
 public class Main {
-	
+
 	//--------------------------------------------------------------------------------------
 	public boolean registerEquipment(String name, String description) { //oppg 1
 		return UseDB.addRow("equipment", name, description);
 	}
-	
+
 	public boolean registerExercise(String name, String description) { //oppg 1
 		return UseDB.addRow("exercise", name, description);
 	}
-	
+
 	//datetime format: yyyy-mm-dd tt:mm:ss, length format: tt:mm:ss
 	public void registerWorkout(String datetime, String length, String info, int shape, int performance, String note) {
 		UseDB.addRow("workout", datetime, length, info, shape, performance, note);
 	}
-	
+
 	public void registerGroup(String name, String description) { //oppg 4
 		UseDB.addRow("ex_group",name,description);
 	}
-	
+
 	public ArrayList<ArrayList<String>> latestWorkouts(int n) { //oppg 2
-		ArrayList<ArrayList<String>> res = UseDB.getTable("SELECT * FROM workout WHERE date_time < CURDATE() ORDER by (date_time) DESC limit 0,"+n+";"); 
+		ArrayList<ArrayList<String>> res = UseDB.getTable("SELECT * FROM workout WHERE date_time < CURDATE() ORDER by (date_time) DESC limit 0,"+n+";");
 		return res;
 	}
-	
+
 	//datetime format: yyyy-mm-dd tt:mm:ss, length format: tt:mm:ss
 	public ArrayList<ArrayList<String>> resultLogs(String exercise, String startDatetime, String endDatetime) { //oppg 3
 		ArrayList<ArrayList<String>> res = UseDB.getTable("SELECT DISTINCT name, date_time,"
@@ -42,23 +42,30 @@ public class Main {
 				+ endDatetime + "'");
 		return res;
 	}
-	
-	public ArrayList<ArrayList<String>> similarExercises() { //oppg 4
-		 ArrayList<ArrayList<String>> similarExercises = UseDB.getTable("SQL spï¿½rring"); //feil sql syntax
-			return similarExercises;
+
+	//Gir ut alle øvelser som er i samme ex_group
+	public ArrayList<String> similarExercises(String exGroupName) { //oppg 4
+		String sql = "SELECT ex_name FROM ex_in_exgroup WHERE group_name = '"+exGroupName+"'";
+		ArrayList<ArrayList<String>> similarExercises = UseDB.getTable(sql);
+		ArrayList<String> Result = new ArrayList<>();
+		for (int i = 0; i<similarExercises.size(); i++) {
+			Result.add(similarExercises.get(i).get(0));
 		}
-	public ArrayList<ArrayList<String>> numberOfWorkouts() { //oppg 4
+			return Result;
+	}
+
+	public ArrayList<ArrayList<String>> numberOfWorkouts() { //oppg 5
 		ArrayList<ArrayList<String>> numberOfWorkouts = UseDB.getTable("SELECT COUNT(*) FROM workout"); //denne funker:)
 			return numberOfWorkouts;
-		}	
+		}
 	//--------------------------------------------------------------------------------------
-	
+
 	// Text based app interface
 	public void textAppLoop() {
 		Scanner scan = new Scanner(System.in);
 		String in = "";
 		String arg1, arg2, arg3, arg4, arg5, arg6;
-		
+
 		printInfo();
 		while (true) {
 			try {
@@ -68,7 +75,7 @@ public class Main {
 				System.out.println("Invalid input");
 			}
 			switch (in) {
-			
+
 				case "REGEQ":
 					System.out.print("Name: ");
 					arg1 = scan.nextLine();
@@ -76,7 +83,7 @@ public class Main {
 					arg2 = scan.nextLine();
 					registerEquipment(arg1, arg2);
 					break;
-					
+
 				case "REGEX":
 					System.out.print("Name: ");
 					arg1 = scan.nextLine();
@@ -84,7 +91,7 @@ public class Main {
 					arg2 = scan.nextLine();
 					registerExercise(arg1, arg2);
 					break;
-					
+
 				case "REGWORK":
 					System.out.print("Workout datetime (yyyy-mm-dd tt:mm:ss): ");
 					arg1 = scan.nextLine();
@@ -100,7 +107,7 @@ public class Main {
 					arg6 = scan.nextLine();
 					registerWorkout(arg1, arg2, arg3, Integer.parseInt(arg4), Integer.parseInt(arg5), arg6);
 					break;
-					
+
 				case "REGGROUP":
 					System.out.print("Name: ");
 					arg1 = scan.nextLine();
@@ -108,7 +115,7 @@ public class Main {
 					arg2 = scan.nextLine();
 					registerGroup(arg1, arg2);
 					break;
-					
+
 				case "RESLOG":
 					System.out.print("Exercise: ");
 					arg1 = scan.nextLine();
@@ -118,28 +125,28 @@ public class Main {
 					arg3 = scan.nextLine();
 					System.out.println(resultLogs(arg1, arg2, arg3));
 					break;
-					
+
 				case "INFO":
 					printInfo();
 					break;
 				case "HELP":
 					printInfo();
 					break;
-					
+
 				case "QUIT":
 					System.out.println("Goodbye");
 					System.exit(0);
 				case "EXIT":
 					System.out.println("Goodbye");
 					System.exit(0);
-					
+
 				default:
 					System.out.println("Not a command");
-			
+
 			}
 		}
 	}
-	
+
 	private static void printInfo() {
 		System.out.println("This is a text based workout app.\n"
 				+ "COMMANDS:\n"
@@ -152,13 +159,13 @@ public class Main {
 				+ "   - Get this info again: INFO or HELP\n"
 				+ "   - Exit of of the app: QUIT or EXIT\n");
 	}
-	
+
 	public static void main(String[] args) {
 		Main m = new Main();
+		System.out.println(m.similarExercises("Brystøvelser"));
+		//System.out.println(m.resultLogs("Benkpress", "2018-02-01 00:00:00", "2018-05-01 00:00:00"));
 
-		System.out.println(m.resultLogs("Benkpress", "2018-02-01 00:00:00", "2018-05-01 00:00:00"));
-		
-		m.textAppLoop();
+		//m.textAppLoop();
 	}
 
 }
